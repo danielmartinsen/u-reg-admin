4fdff8f17af2eec06776d63e3bf1e63be60e6c42
Daniel Martinsen <daniel.martinsen@online.no>
2020-05-10 19:40:03 +0200

    Update customer table

--- components/table.js
+++ components/table.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import { loadCSS } from 'fg-loadcss'
 
+import Box from '@material-ui/core/Box'
+import Collapse from '@material-ui/core/Collapse'
+import IconButton from '@material-ui/core/IconButton'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
 import TableCell from '@material-ui/core/TableCell'
@@ -8,51 +10,106 @@ import TableContainer from '@material-ui/core/TableContainer'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 import Paper from '@material-ui/core/Paper'
-import Icon from '@material-ui/core/Icon'
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown'
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp'
 
 import styles from '../styles/table.module.scss'
 
-function createData(navn, kommune, gyldighet, domene) {
-  return { navn, kommune, gyldighet, domene }
+function Row({ row }) {
+  const [open, setOpen] = React.useState(false)
+
+  return (
+    <React.Fragment>
+      <TableRow>
+        <TableCell>
+          <IconButton aria-label='expand row' size='small' onClick={() => setOpen(!open)}>
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell>{row.navn}</TableCell>
+        <TableCell>{row.kommune}</TableCell>
+        <TableCell>{row.gyldighet}</TableCell>
+        <TableCell>
+          <a href={row.domene}>{row.domene}</a>
+        </TableCell>
+      </TableRow>
+
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout='auto' unmountOnExit>
+            <Box margin={1}>
+              <div className={styles.infoBox}>
+                <div>
+                  <h2>Informasjon</h2>
+                  {row.lisens.map((lisensRow) => (
+                    <p key={`${row.navn} "-lisens"`}>
+                      <b>Lisens:</b> {lisensRow.key} <i>(Utgår {lisensRow.dato})</i>
+                    </p>
+                  ))}
+                  <p>
+                    <b>Fakturaadresse:</b> {row.fakturaadresse}
+                  </p>
+                </div>
+
+                <div>
+                  <h2>Kontaktperson</h2>
+                  {row.kontaktperson.map((kontaktpersonRow) => (
+                    <div key={`${row.navn} "-kontaktperson"`}>
+                      <p>
+                        <b>Navn:</b> {kontaktpersonRow.navn}
+                      </p>
+                      <p>
+                        <b>E-post:</b> {kontaktpersonRow.epost}
+                      </p>
+                      <p>
+                        <b>Telefon:</b> {kontaktpersonRow.telefon}
+                      </p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+
+              <button className={styles.kundeBtn}>Endre</button>
+              <button className={styles.kundeBtn}>Forny</button>
+              <button className={styles.kundeBtn}>Slett</button>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  )
 }
 
 const rows = [
-  createData('Midtgard Ungdomshus', 'Ås Kommune', 'Gyldig', 'https://br-reg-midtgard.now.sh'),
-  createData('Brakka', 'Nordre Follo Kommune', 'Ugyldig', 'https://br-reg-brakka.now.sh'),
+  {
+    navn: 'Midtgard Ungdomshus',
+    kommune: 'Ås Kommune',
+    gyldighet: 'Gyldig',
+    domene: 'https://br-reg-midtgard.now.sh',
+    fakturaadresse: 'Skoleveien 1, 1435 Ås',
+    kontaktperson: [
+      { navn: 'Nicklas Erdal', epost: 'nicklas.erdal.as.kommune.no', telefon: '95533123' },
+    ],
+    lisens: [{ key: 'j4b1FLkDQtSITyNMn2KDkAKFQrt5pNUm', dato: '09.05.2021' }],
+  },
 ]
 
 export default function kundeTable() {
-  React.useEffect(() => {
-    loadCSS('https://use.fontawesome.com/releases/v5.12.0/css/all.css', document.querySelector('#font-awsome-css'))
-  }, [])
-
   return (
     <TableContainer component={Paper}>
-      <Table aria-label='simple table'>
+      <Table>
         <TableHead className={styles.tableHeader}>
           <TableRow>
+            <TableCell />
             <TableCell>Navn</TableCell>
             <TableCell>Kommune</TableCell>
             <TableCell>Gyldighet</TableCell>
             <TableCell>Domene</TableCell>
-            <TableCell></TableCell>
           </TableRow>
         </TableHead>
         <TableBody className={styles.tableBody}>
           {rows.map((row) => (
-            <TableRow key={row.navn}>
-              <TableCell>{row.navn}</TableCell>
-              <TableCell>{row.kommune}</TableCell>
-              <TableCell>{row.gyldighet}</TableCell>
-              <TableCell>
-                <a href={row.domene}>{row.domene}</a>
-              </TableCell>
-              <TableCell align='right'>
-                <Icon className='fa fa-info-circle' />
-                <Icon className='fa fa-sync-alt' />
-                <Icon className='fa fa-trash' />
-              </TableCell>
-            </TableRow>
+            <Row key={row.name} row={row} />
           ))}
         </TableBody>
       </Table>
